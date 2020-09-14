abd9585ea728ef3434bd6ceda5f20af230c882fc
sychus <hfernandez@neuquen.gov.ar>
2020-09-14 08:33:06 -0300

    change in coonection

--- src/connect.ts
+++ src/connect.ts
@@ -1,18 +1,11 @@
 import mongoose from "mongoose";
 
-export default (db: string) => {
-  const connect = () => {
-    mongoose
-      .connect(db, { useNewUrlParser: true })
-      .then(() => {
-        return console.log(`Se conecto a la base de datos correctamente`);
-      })
-      .catch(error => {
-        console.log("Error al conectarse a la base de datos: ", error);
-        return process.exit(1);
-      });
-  };
-  connect();
-
-  mongoose.connection.on("disconnected", connect);
-};
+export default async (db: string) => {
+  try {
+      await mongoose.connect(db)
+      console.info(`Connected to database on Worker process: ${process.pid}`)
+  } catch (error) {
+      console.error(`Connection error: ${error.stack} on Worker process: ${process.pid}`)
+      process.exit(1)
+  }
+}
