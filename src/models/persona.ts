9085d727c24312d3b0cc2f9fc7b36d2b6588a615
sychus <hfernandez@neuquen.gov.ar>
2020-09-23 12:30:18 -0300

    fix(model): Agrego interface

--- src/models/persona.ts
+++ src/models/persona.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import { IPersona } from '../interfaces/persona';
 
 const schema = new Schema({
   nombre: { type: String, required: true },
@@ -8,5 +9,5 @@ const schema = new Schema({
   activo: {type: Boolean, required: true}
 });
 
-export const PersonaModel = mongoose.model('persona', schema);
+export const PersonaModel = mongoose.model<IPersona>('persona', schema);
 export default PersonaModel;
