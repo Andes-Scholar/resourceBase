2d6f6cc58fcf9564a3f53f014a0478e8bcd32f96
sychus <hfernandez@neuquen.gov.ar>
2020-09-25 09:51:44 -0300

    fix(controller): modificamos las busquedas parciales

--- src/controllers/persona.ts
+++ src/controllers/persona.ts
@@ -5,10 +5,12 @@ class PersonaResource extends ResourceBase {
     Model = PersonaModel;
     resourceName = 'personas';
     searchFileds = {
-        nombre: (value: string) => MongoQuery.partialString(`^${value}`),
-        apellido: (value: string) => MongoQuery.partialString(`^${value}`),
+        // TIP: Para partialString, tener en cuenta desde la app o desde postman enviar la expresión para realizar la búsqueda.
+        // Ejemplo: `/personas?nombre=^${stringDeBusqueda}`) (desde el servicio de la app)
+        nombre: MongoQuery.partialString,
+        apellido: MongoQuery.partialString,
         activo: MongoQuery.equalMatch,
-        email:  (value: string) => MongoQuery.partialString(`^${value}`),
+        email: MongoQuery.partialString,
         documento: MongoQuery.equalMatch
     };
 }
